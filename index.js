6626da72fe78dcc2975dc7c1ac36e336bcf7a5ab
vladkesia <kesyawork96@gmail.com>
2023-01-10 11:28:39 +0200

    fix All bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function searchNumber(arr) {
     let sum = 0
-    let midle = 1
+    let midle = 0
     if (Array.isArray(arr)) {
         arr.map(element => {
             if (!isNaN(element) && typeof element === 'number') {
@@ -12,89 +12,108 @@ function searchNumber(arr) {
     return sum / midle
 }
 
-const array = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10]
+const array = [2, 3, 3, 5, 7, 10]
 console.log(searchNumber(array))
 
-function doMatch(x, znak, y) {
-    const firstNumber = Number(x)
-    const secondNumber = Number(y)
-    let result
-    if (!isNaN(x) && !isNaN(y)) {
-        switch (znak) {
-            case '+':
-                result = firstNumber + secondNumber
-                break;
-            case '-':
-                result = firstNumber - secondNumber
-                break;
-            case '*':
-                result = firstNumber * secondNumber
-                break;
-            case '/':
-                result = firstNumber / secondNumber
-                break;
-            case '%':
-                result = firstNumber % secondNumber
-                break;
-            case '^':
-                result = firstNumber ^ secondNumber
-                break;
-            default:
-                result = NaN
-        }
-    }
-    return result
-}
 
-console.log(doMatch(7,'*',2));
+let input2 = prompt('введіть ваше вираження без пробілів все разом ')
 
-
-function searchSymbol(sentence, symbols) {
-   if (Array.isArray(symbols) && typeof sentence === 'string') {
-        for(let i = 0; i<symbols.length;i++){
-            if(typeof symbols[i] === 'number'){
-                return new Error('в symbols передаеться массив string')
+function doMatch(value) {
+    if (value !== null && value.trim() !== '' && value.length === 3) {
+        const valueArr = value.split('')
+        const firstNumber = Number(valueArr[0])
+        const secondNumber = Number(valueArr[2])
+        const znak = valueArr[1]
+        let result
+        if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
+            switch (znak) {
+                case '+':
+                    result = firstNumber + secondNumber
+                    break;
+                case '-':
+                    result = firstNumber - secondNumber
+                    break;
+                case '*':
+                    result = firstNumber * secondNumber
+                    break;
+                case '/':
+                    result = firstNumber / secondNumber
+                    break;
+                case '%':
+                    result = firstNumber % secondNumber
+                    break;
+                case '^':
+                    result = firstNumber ** secondNumber
+                    break;
+                default:
+                    result = NaN
             }
         }
-       for(const elem of symbols) {
-           sentence = sentence.split(elem).join('');
-       }
-       return  sentence
-}
+        alert(result)
+    } else {
+        alert('треба було ввести перше число знак(що треба зробити ) друге число')
+    }
 }
 
-let test = 'Hello word'
-console.log(searchSymbol(test, ['w','l']));
+doMatch(input2)
 
-// const input = prompt('Введіть довжину массиву, два числа через кому')
+ const input = prompt('Введіть довжину массиву, два числа через кому')
+
 function userArray(input) {
-  if(input?.trim()) {
-      const arr = input.split(',')
-      const sum = +arr[0] + +arr[1]
-      if(arr.length === 2 && Number(arr[0]) && Number(arr[1])) {
-          const inputArr = prompt(`Введіть ${sum} символів через кому `).split(',')
-          if( inputArr !== null && inputArr.length >0 && inputArr.length === sum) {
-              const firstArray = []
-              const secondArray = []
-              for (let i = 0; i < arr[0]; i++) {
-                  firstArray.push(inputArr[i])
-              }
-              inputArr.splice(0, +arr[0])
-              for (let i = 0; i < arr[1]; i++) {
-                  secondArray.push(inputArr[i])
-              }
-              const resultArr = [firstArray, secondArray]
-              console.log(resultArr)
-          }else {
-              alert(`Треба було ввесты ${sum} `)
-          }
-      }else {
-          alert('треба ввести дві цифри через кому ')
-      }
-  }else {
-      return alert('ви нічого не ввели або відмінили дію' )
-  }
+    const arr = input.split(',')
+    let sum = 0
+    for (let element of arr){
+        if (!Number(element)){
+            return alert("повинно бути число і повинно бути більше 0  ")
+        }else {
+            sum +=Number(element)
+        }
+    }
+    const inputUserArray = prompt(`Введіть ${sum} єлементи массиву`)
+    if(inputUserArray !== null && inputUserArray.trim() !== '') {
+        const sience = inputUserArray.split(',')
+        if(sience.length === sum) {
+            const resultArr = []
+            let resultString = ''
+            for (let i = 0; i < arr.length; i++) {
+                const array = []
+                for (let j = 0; j < arr[+i]; j++) {
+                    array.push(sience[j])
+                }
+                resultArr.push(array)
+                resultString += `[${array}]`
+                sience.splice(0, array.length)
+            }
+            console.log(resultArr)
+            alert(`[${resultString}]`)
+        }else {
+            alert(`колколичество элементов должно быть ${sum} `)
+        }
 
+    }else {alert('Треба ввести числа через кому ')}
 }
-// userArray(input)
 
+userArray(input)
+
+const firstInput = prompt('введіть речення')
+const secondInput = prompt('Ведіть через пробіл символи які треба видалити')
+
+function searchSymbol(sentence, symbols) {
+    if (sentence !== null && sentence.trim() !== '' && symbols !== null && symbols.trim() !== '') {
+        const array = symbols.split(' ')
+        for (let i = 0; i < array.length; i++) {
+            if (array[i].length !== 1) {
+                return alert('Треба по одному символу через пробіл')
+            }
+        }
+        for (const elem of array) {
+            sentence = sentence.split(elem).join('');
+        }
+        alert(sentence)
+    } else {
+        alert('Треба було щось ввести')
+    }
+}
+
+
+searchSymbol(firstInput, secondInput)
